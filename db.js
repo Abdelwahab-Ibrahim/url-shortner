014305df1662eadc04fb8072aeb2bfdfa5716bf9
Abd-elwahab Ibrahim <abdelwahab.ibn.ibrahim@gmail.com>
2026-04-18 18:39:17 +0200

    db.js pretty much complete

--- db.js
+++ db.js
@@ -3,13 +3,15 @@ const { MongoVlint, MongoClient } = require('mongodb');
 let dbconnection;
 
 module.exports = {
-    connectToDb: () => {
+    connectToDb: (cb) => {
         MongoClient.connect(`mongodb://localhost:27017/links`)
             .then((clint) => {
                 dbconnection = clint.db();
+                return cb;
             }).catch(err => {
                 console.log(err);
+                return cb(err);
             })
     },
-    getDb: () => { }
+    getDb: () => dbconnection
 };
